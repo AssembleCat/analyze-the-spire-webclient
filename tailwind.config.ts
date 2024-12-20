33531c2d06f0e669f9980dcdad5b14e90b7ec80c
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-20 14:48:26 +0900

    Tailwind CSS 캐릭터별 색상 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,13 @@
 import type { Config } from "tailwindcss";
 
+const characterColors = [
+  "ironclad",
+  "silent",
+  "defect",
+  "watcher",
+  "colorless",
+];
+
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +19,14 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        ironclad: "#e6340e",
+        silent: "#2ECC71",
+        defect: "#33A8FF",
+        watcher: "#9B59B6",
+        colorless: "#828282",
       },
     },
   },
+  safelist: characterColors.map((color) => `bg-${color}`),
   plugins: [],
 } satisfies Config;
