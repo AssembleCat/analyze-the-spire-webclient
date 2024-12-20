5fe0e0dc33edcf5921cc80148f84e6b6e64c2c6f
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-20 14:25:02 +0900

    Header Rounting 추가가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,47 @@
-export default function Header() {
-    return (
-        <header className="bg-gray-800 text-white py-8 px-6 flex justify-between items-center">
-            <h1 className="text-2xl font-bold">Analyze the Spire</h1>
-            <nav>
-                <ul className="flex space-x-4">
-                    <li><a href="#" className="hover:text-gray-300">Home</a></li>
-                    <li><a href="#" className="hover:text-gray-300">Cards</a></li>
-                    <li><a href="#" className="hover:text-gray-300">Decks</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+"use client";
+import { useRouter } from "next/navigation";
 
+export default function Header() {
+  const router = useRouter();
+  return (
+    <header className="bg-gray-800 text-white py-8 px-6 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">Analyze the Spire</h1>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <button
+              onClick={() => router.push("/")}
+              className="hover:text-gray-300"
+            >
+              Predict
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => router.push("/post")}
+              className="hover:text-gray-300"
+            >
+              Post
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => router.push("/stats")}
+              className="hover:text-gray-300"
+            >
+              Stats
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => router.push("/tier")}
+              className="hover:text-gray-300"
+            >
+              Tier
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
