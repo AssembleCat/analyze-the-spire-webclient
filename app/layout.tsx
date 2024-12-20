ed1e43cd0237660dd9f71956559b2d11bf992a50
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-20 15:39:47 +0900

    Header 화면고정정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
-    title: {
-        template: "%s | Analyze the Spire",
-        default: "Analyze the Spire"
-    },
-    description: "Slay the Spire with a deep learning model",
+  title: {
+    template: "%s | Analyze the Spire",
+    default: "Analyze the Spire",
+  },
+  description: "Slay the Spire with a deep learning model",
 };
 
-export default function RootLayout({children}: Readonly<{
-    children: React.ReactNode;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <body>
-        <Header />
-        {children}
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>
+        <div className="h-screen flex flex-col">
+          <Header />
+
+          <main className="flex-1 overflow-auto"> {children}</main>
+        </div>
+      </body>
+    </html>
+  );
 }
