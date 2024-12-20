eb26bc21eea1d0f9937c770ebb680b1ba39496a2
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-20 18:28:10 +0900

    [fix] Header의 Navigation생성 변경경

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,45 +1,30 @@
 "use client";
 import { useRouter } from "next/navigation";
 
+const navigation = {
+  Predict: "/",
+  Post: "/post",
+  Stats: "/stats",
+  Tier: "/tier",
+};
+
 export default function Header() {
   const router = useRouter();
   return (
-    <header className="bg-gray-800 text-white py-8 px-6 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Analyze the Spire</h1>
+    <header className="bg-gray-800 text-white py-6 px-6 flex justify-between items-center">
+      <h1 className="ml-10 text-4xl font-bold">Analyze the Spire</h1>
       <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <button
-              onClick={() => router.push("/")}
-              className="hover:text-gray-300"
-            >
-              Predict
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => router.push("/post")}
-              className="hover:text-gray-300"
-            >
-              Post
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => router.push("/stats")}
-              className="hover:text-gray-300"
-            >
-              Stats
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => router.push("/tier")}
-              className="hover:text-gray-300"
-            >
-              Tier
-            </button>
-          </li>
+        <ul className="mr-10 flex space-x-4">
+          {Object.entries(navigation).map(([label, path]) => (
+            <li key={label}>
+              <button
+                onClick={() => router.push(path)}
+                className="hover:text-gray-400"
+              >
+                <h1 className="text-xl">{label}</h1>
+              </button>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
